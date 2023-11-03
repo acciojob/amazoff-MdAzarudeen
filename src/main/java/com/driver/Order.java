a8474d35fe642d29dbc5993ecf671815cf7caaed
S <c8949053@lowes.com>
2023-11-03 10:19:16 +0530

    changes 18

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,7 +9,15 @@ public class Order {
         this.id = id;
 
         // The deliveryTime has to converted from string to int and then stored in the attribute deliveryTime  = HH*60 + MM
-        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0, 2)) * 60 + Integer.parseInt(deliveryTime.substring(3));
+//        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0, 2)) * 60 + Integer.parseInt(deliveryTime.substring(3));
+        if(deliveryTime.length() != 0){
+            String hrs = deliveryTime.substring(0, 2);
+            String mnts = deliveryTime.substring(3);
+            this.deliveryTime = Integer.parseInt(hrs)*60 + Integer.parseInt(mnts);
+        }
+        else{
+            this.deliveryTime = 0;
+        }
     }
 
     public String getId() {
